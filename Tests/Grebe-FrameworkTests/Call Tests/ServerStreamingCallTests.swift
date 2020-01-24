0bb52f1a46cc01bad746af2704481e48defd42ef
Tim Mewe <tim.mewe@me.com>
2020-01-24 08:34:59 +0100

    server streaming call: test failed precondition

--- Tests/Grebe-FrameworkTests/Call Tests/ServerStreamingCallTests.swift
+++ Tests/Grebe-FrameworkTests/Call Tests/ServerStreamingCallTests.swift
@@ -55,4 +55,32 @@ final class ServerStreamingCallTests: BaseCallTest {
         
         wait(for: [serverStreamingMock.expectation, responseExpectation], timeout: 0.1, enforceOrder: true)
     }
+    
+    func testFailedPrecondition() {
+        let expectedRequest = EchoRequest(id: 1)
+        let expectedResponse: GRPCStatus = .init(code: .failedPrecondition, message: nil)
+        let serverStreamingMock = UnaryMock<EchoRequest, EchoResponse>(
+            request: expectedRequest,
+            response: .failure(expectedResponse)
+        )
+        
+        mockClient.mockNetworkCalls = [serverStreamingMock]
+        let errorExpectation = XCTestExpectation(description: "Correct Error")
+        
+        let call = GServerStreamingCall(request: expectedRequest, closure: mockClient.test)
+        call.execute()
+            .sink(receiveCompletion: {
+                switch $0 {
+                case .failure(let status):
+                    XCTAssertEqual(status, expectedResponse)
+                    errorExpectation.fulfill()
+                case .finished:
+                    XCTFail("Call should fail")
+                }
+            }, receiveValue: { _ in
+                XCTFail("Call should fail")
+            }).store(in: &cancellables)
+        
+        wait(for: [serverStreamingMock.expectation, errorExpectation], timeout: 0.1, enforceOrder: true)
+    }
 }
