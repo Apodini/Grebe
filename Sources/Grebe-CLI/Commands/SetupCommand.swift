dd952d995a3dff8b118427fd58ee997cae2146ee
Tim Mewe <tim.mewe@me.com>
2020-02-18 08:23:59 +0100

    homebrew install message

--- Sources/Grebe-CLI/Commands/SetupCommand.swift
+++ Sources/Grebe-CLI/Commands/SetupCommand.swift
@@ -26,22 +26,26 @@ internal class SetupCommand: IExecutableCommand {
     // MARK: - IExecutableCommand
     
     func run() throws {
+        print("""
+        We use Homebrew to install Grebe. Please make sure you have Homebrew installed.
+        If not you can download it here: https://brew.sh
+        """)
         // Install protobuf via brew
         try shell("reinstall", "protobuf", launchPath: "\(envPath)/brew")
-        
+
         // Install swift protobuf via brew
         try shell("reinstall", "swift-protobuf", launchPath: "\(envPath)/brew")
-        
+
         // Checkout grpc-swift repo
         try shell("git", "clone", "--single-branch", "--branch", "nio", grpcURL, repoPath)
-        
+
         // Make plugins
         try shell("make", "plugins", "--directory", repoPath)
-        
+
         // Move plugins to path
         try moveFile(from: repoPath + "/protoc-gen-grpc-swift", to: envPath + "/protoc-gen-grpc-swift")
         try moveFile(from: repoPath + "/protoc-gen-swift", to: envPath + "/protoc-gen-swift")
-        
+
         // Delete grpc-swift repo
         try FileManager.default.removeItem(atPath: repoPath)
     }
